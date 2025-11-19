5b3a6f4d6e43927ef89839293be2b95d01e57135
Augustinas <blogas190@gmail.com>
2025-11-19 22:55:01 +0900

    Update

---
+++ Assets/Imports/ShinySSRR/Runtime/Scripts/SkyboxBaker.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ShinySSRR {
+
+    [ExecuteAlways]
+    public class SkyboxBaker : MonoBehaviour {
+
+        public static bool needSkyboxUpdate = true;
+        RenderTexture skyboxCubemap;
+        float lastSkyboxSnapshotTime;
+        ShinyScreenSpaceRaytracedReflections settings;
+        Camera cam;
+        RenderTextureFormat currentSkyboxFormat;
+
+        void OnEnable() {
+            needSkyboxUpdate = true;
+            cam = GetComponent<Camera>();
+        }
+
+        private void OnDestroy() {
+            ReleaseSkyboxCubemap();
+        }
+
+
+        void LateUpdate() {
+            settings = VolumeManager.instance?.stack?.GetComponent<ShinyScreenSpaceRaytracedReflections>();
+
+            if (settings == null || settings.skyboxIntensity.value <= 0 || cam == null) return;
+
+            if (settings.skyboxUpdateMode.value == SkyboxUpdateMode.Interval && Time.time - lastSkyboxSnapshotTime >= settings.skyboxUpdateInterval.value) {
+                lastSkyboxSnapshotTime = Time.time;
+                needSkyboxUpdate = true;
+            }
+
+            if (needSkyboxUpdate && cam.cameraType == CameraType.Game) {
+                needSkyboxUpdate = false;
+                UpdateSkyboxCubemap();
+            }
+        }
+
+        void ReleaseSkyboxCubemap() {
+            if (skyboxCubemap == null) return;
+            skyboxCubemap.Release();
+            DestroyImmediate(skyboxCubemap);
+        }
+
+        public void UpdateSkyboxCubemap() {
+
+            if (settings.skyboxUpdateMode.value == SkyboxUpdateMode.CustomCubemap) {
+                Shader.SetGlobalTexture(ShaderParams.SkyboxCubemap, settings.skyboxCustomCubemap.value);
+                return;
+            }
+
+            if (cam == null) return;
+
+            int cullingMask = cam.cullingMask;
+            float farClipPlane = cam.farClipPlane;
+
+            int desiredCullingMask = settings.skyboxCullingMask.value;
+            cam.cullingMask = desiredCullingMask;
+            if (desiredCullingMask == 0) {
+                cam.farClipPlane = cam.nearClipPlane + 0.1f;
+            } else {
+                cam.farClipPlane = farClipPlane;
+            }
+
+            int res = (int)Mathf.Pow(2, (int)settings.skyboxResolution.value + 4);
+            res = Mathf.Clamp(res, 16, 8192);
+            RenderTextureFormat rtFormat = settings.skyboxHDR.value ? RenderTextureFormat.DefaultHDR : RenderTextureFormat.Default;
+            if (skyboxCubemap == null || skyboxCubemap.width != res || currentSkyboxFormat != rtFormat) {
+                currentSkyboxFormat = rtFormat;
+                ReleaseSkyboxCubemap();
+                skyboxCubemap = new RenderTexture(res, res, 0, rtFormat);
+                skyboxCubemap.dimension = TextureDimension.Cube;
+            }
+            cam.RenderToCubemap(skyboxCubemap);
+            Shader.SetGlobalTexture(ShaderParams.SkyboxCubemap, skyboxCubemap);
+
+            cam.cullingMask = cullingMask;
+            cam.farClipPlane = farClipPlane;
+        }
+
+
+    }
+
+}
