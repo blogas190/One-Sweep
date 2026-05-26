4e535a6f291fe9fa397237c64b45b686a9ab4f70
Augustinas <blogas190@gmail.com>
2026-05-27 02:12:45 +0900

    Updates of the week

---
+++ Assets/Imports/City PACK/Scripts/LightMapData.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class LightMapData : MonoBehaviour {
+	
+	[System.Serializable]
+	public struct RendererInfo
+	{
+		public Renderer 	renderer;
+		public int 			lightmapIndex;
+		public Vector4 		lightmapOffsetScale;
+	}
+
+	[SerializeField]
+	 public RendererInfo	m_RendererInfo;
+	
+
+	public void GetInfo()
+	{
+		m_RendererInfo.renderer = GetComponent<Renderer> ();
+		if (m_RendererInfo.renderer) 
+		{
+			m_RendererInfo.lightmapIndex = m_RendererInfo.renderer.lightmapIndex;
+			m_RendererInfo.lightmapOffsetScale = m_RendererInfo.renderer.lightmapScaleOffset;
+		}
+
+	}
+}
